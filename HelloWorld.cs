a117ca382ae62d0caad90ebe40bd7fd5535e58af
Dwayne <Dwayne@DWAYNE-PC>
2023-12-03 18:22:09 -0500

    Add project files.

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    //created a new class for our Hello World program.
+    internal class HelloWorld
+    {
+        //create a method that produces the string "Hello World!".
+
+        public static string Greeting()    //the method is static so that a "new" instance of the class 
+                                           //isn't needed to access this method.
+        {
+            return "Hello World!";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    static class Program 
+    {
+                
+        //The main entry point for the application.
+        static void Main()
+        {
+            //METHOD 1:
+            //You can use classes to tidy up and modularize your code. This makes it easier to reuse than the second method
+            //which hard codes the string "Hello World!" into the application
+
+            //call the HelloWorld Greeting method from its class and display it on the console screen.
+            Console.WriteLine(HelloWorld.Greeting());
+
+
+            //METHOD 2:
+            //An example of hard coding the "Hello World!" string. It is fewer lines of code but
+            //it is not as modular.
+
+            Console.WriteLine("\nHello World Again!");
+
+        }
+    }    
+}
